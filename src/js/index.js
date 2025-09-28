c3becccd2e283288520adde14b33aff74af8f620
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2025-09-28 11:08:50 -0600

    Add basic header layout and event control

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,25 @@
 console.log("Hi, Mom!");
 
 import "../css/style.css";
+
+document.addEventListener("click", (e) => {
+    let target = e.target;
+    
+    switch(target.id) {
+        case "settingsBtn":
+            console.log("settings button clicked");
+            break;
+        case "settingTempBtn":
+            console.log("temperature toggle clicked");
+            break;
+        case "settingSpeedBtn":
+            console.log("speed toggle clicked");
+            break;
+        case "settingModeBtn":
+            console.log("mode toggle clicked");
+            break;
+        case "searchBtn":
+            console.log("search button clicked");
+            break;
+    }
+});
