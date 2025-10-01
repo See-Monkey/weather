24e24a91c4a091eb459f8826aeb5b913edc842fc
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2025-09-30 20:46:44 -0600

    Add redrawFuture function

--- src/js/display.js
+++ src/js/display.js
@@ -653,7 +653,7 @@ export default class Display {
         content.innerHTML = "";
 
         // future
-        const future = this.createElement("section", "future");
+        const sectionFuture = this.createElement("section", "future");
         const backBtn = this.createElement("button", "container");
         backBtn.id = "backBtn";
         backBtn.textContent = "Back";
@@ -663,8 +663,321 @@ export default class Display {
         });
 
 
+        const currentHeader = this.createElement('div', 'currentHeader');
+        const currentHeaderMain = this.createElement('div', 'currentHeaderMain');
+        const h2 = this.createElement('h2', 'resolvedAddress', forecast.resolvedAddress);
+        const h4Desc = this.createElement('h4', 'forecastDescription', forecast.description);
+
+        currentHeaderMain.appendChild(h2);
+        currentHeaderMain.appendChild(h4Desc);
+
+        const headerContainer = this.createElement('div', 'container');
+        const latLonIcon = this.createElement('div', 'latLonIcon');
+
+        const latCol = this.createElement('div', 'containerCol');
+        const latTitle = this.createElement('h4', 'title', 'Lat');
+        const latValue = this.createElement('p', 'value', forecast.latitude);
+        latCol.appendChild(latTitle);
+        latCol.appendChild(latValue);
+
+        const lonCol = this.createElement('div', 'containerCol');
+        const lonTitle = this.createElement('h4', 'title', 'Lon');
+        const lonValue = this.createElement('p', 'value', forecast.longitude);
+        lonCol.appendChild(lonTitle);
+        lonCol.appendChild(lonValue);
+
+        headerContainer.appendChild(latLonIcon);
+        headerContainer.appendChild(latCol);
+        headerContainer.appendChild(lonCol);
+
+        currentHeader.appendChild(currentHeaderMain);
+        currentHeader.appendChild(headerContainer);
+
+        // --- containerWrap: currentDate & Temp ---
+        const containerWrap1 = this.createElement('div', 'containerWrap');
+        const currentDate = this.createElement('div', 'currentDate');
+        const today = new Date();
+        const dateIndex = addDays(today, index);
+        const date = this.createElement('h3', 'date', format((dateIndex), "EEEE, MMMM do, yyyy"));
+        const currentDescription = this.createElement('h4', 'currentDescription', forecast.days[index].description);
+
+        currentDate.appendChild(date);
+        currentDate.appendChild(currentDescription);
+
+        const currentConditionIcon = this.createElement('div', 'currentConditionIcon');
+        currentConditionIcon.classList.add(Weather.getConditionIcon(forecast.days[index].icon))
+
+        const tempContainer = this.createElement('div', 'tempContainer');
+        const tempIcon = this.createElement('div', 'tempIcon');
+        tempIcon.classList.add(Weather.getTempIcon(forecast.days[index].temp));
+
+        const tempMaxMin = this.createElement('div', 'tempMaxMin');
+
+        // High
+        const highCol = this.createElement('div', 'containerCol');
+        const highTitle = this.createElement('h4', 'title', 'High');
+        const highValueHolder = this.createElement('div', 'valueHolder');
+        const highValue = this.createElement('p', 'value', this.tempToggle === 0 ? Math.trunc(forecast.days[index].tempmax) : this.convertC(forecast.days[index].tempmax));
+        const highUnit = this.createElement('p', 'unit', this.tempToggle === 0 ? '°F' : '°C');
+        highValueHolder.appendChild(highValue);
+        highValueHolder.appendChild(highUnit);
+        highCol.appendChild(highTitle);
+        highCol.appendChild(highValueHolder);
+
+        // Low
+        const lowCol = this.createElement('div', 'containerCol');
+        const lowTitle = this.createElement('h4', 'title', 'Low');
+        const lowValueHolder = this.createElement('div', 'valueHolder');
+        const lowValue = this.createElement('p', 'value', this.tempToggle === 0 ? Math.trunc(forecast.days[index].tempmin) : this.convertC(forecast.days[index].tempmin));
+        const lowUnit = this.createElement('p', 'unit', this.tempToggle === 0 ? '°F' : '°C');
+        lowValueHolder.appendChild(lowValue);
+        lowValueHolder.appendChild(lowUnit);
+        lowCol.appendChild(lowTitle);
+        lowCol.appendChild(lowValueHolder);
+
+        tempMaxMin.appendChild(highCol);
+        tempMaxMin.appendChild(lowCol);
+
+        tempContainer.appendChild(tempIcon);
+        tempContainer.appendChild(tempMaxMin);
+
+        containerWrap1.appendChild(currentDate);
+        containerWrap1.appendChild(currentConditionIcon);
+        containerWrap1.appendChild(tempContainer);
+
+        // --- containerWrap precipOpt ---
+        const containerWrap2 = this.createElement('div', 'containerWrap precipOpt');
+        const cloudPrecipContainer = this.createElement('div', 'container');
+
+        const cloudIcon = this.createElement('div', 'cloudCoverIcon');
+        const cloudCol = this.createElement('div', 'containerCol');
+        const cloudTitle = this.createElement('h4', 'title', 'Cloud');
+        const cloudValueHolder = this.createElement('div', 'valueHolder');
+        const cloudValue = this.createElement('p', 'value', forecast.days[index].cloudcover);
+        const cloudUnit = this.createElement('p', 'unit', '%');
+        cloudValueHolder.appendChild(cloudValue);
+        cloudValueHolder.appendChild(cloudUnit);
+        cloudCol.appendChild(cloudTitle);
+        cloudCol.appendChild(cloudValueHolder);
+
+        const precipIcon = this.createElement('div', 'precipIcon');
+        const precipCol = this.createElement('div', 'containerCol');
+        const precipTitle = this.createElement('h4', 'title', 'Precip');
+        const precipValueHolder = this.createElement('div', 'valueHolder');
+        const precipValue = this.createElement('p', 'value', forecast.days[index].precipprob);
+        const precipUnit = this.createElement('p', 'unit', '%');
+        precipValueHolder.appendChild(precipValue);
+        precipValueHolder.appendChild(precipUnit);
+        precipCol.appendChild(precipTitle);
+        precipCol.appendChild(precipValueHolder);
+
+        cloudPrecipContainer.appendChild(cloudIcon);
+        cloudPrecipContainer.appendChild(cloudCol);
+        cloudPrecipContainer.appendChild(precipIcon);
+        cloudPrecipContainer.appendChild(precipCol);
+
+        const rainSnowContainer = this.createElement('div', 'container');
+        const rainIcon = this.createElement('div', 'precipRainIcon');
+        const rainCol = this.createElement('div', 'containerCol');
+        const rainTitle = this.createElement('h4', 'title', 'Rain');
+        const rainValueHolder = this.createElement('div', 'valueHolder');
+        const rainValue = this.createElement('p', 'value', this.speedToggle === 0 ? forecast.days[index].precip : this.convertCm(forecast.days[index].precip));
+        const rainUnit = this.createElement('p', 'unit', this.speedToggle === 0 ? "in" : "cm");
+        rainValueHolder.appendChild(rainValue);
+        rainValueHolder.appendChild(rainUnit);
+        rainCol.appendChild(rainTitle);
+        rainCol.appendChild(rainValueHolder);
+
+        const snowIcon = this.createElement('div', 'precipSnowIcon');
+        const snowCol = this.createElement('div', 'containerCol');
+        const snowTitle = this.createElement('h4', 'title', 'Snow');
+        const snowValueHolder = this.createElement('div', 'valueHolder');
+        const snowValue = this.createElement('p', 'value', this.speedToggle === 0 ? forecast.days[index].snow : this.convertCm(forecast.days[index].snow));
+        const snowUnit = this.createElement('p', 'unit', this.speedToggle === 0 ? "in" : "cm");
+        snowValueHolder.appendChild(snowValue);
+        snowValueHolder.appendChild(snowUnit);
+        snowCol.appendChild(snowTitle);
+        snowCol.appendChild(snowValueHolder);
+
+        rainSnowContainer.appendChild(rainIcon);
+        rainSnowContainer.appendChild(rainCol);
+        rainSnowContainer.appendChild(snowIcon);
+        rainSnowContainer.appendChild(snowCol);
+
+        containerWrap2.appendChild(cloudPrecipContainer);
+        containerWrap2.appendChild(rainSnowContainer);
+
+        if (!forecast.days[index].precip > 0 && !forecast.days[index].snow > 0) {
+            rainSnowContainer.style.display = "none";
+        } else if (!forecast.days[index].precip > 0) {
+            rainIcon.style.display = "none";
+            rainCol.style.display = "none";
+        } else if (!forecast.days[index].snow > 0) {
+            snowIcon.style.display = "none";
+            snowCol.style.display = "none";
+        }
+
+        // --- containerWrap: Humidity/Dew & Wind ---
+        const containerWrap3 = this.createElement('div', 'containerWrap');
+
+        // Humidity & Dew
+        const humidityContainer = this.createElement('div', 'container');
+        const humidityIcon = this.createElement('div', 'humidityIcon');
+
+        const humidityCol = this.createElement('div', 'containerCol');
+        const humidityTitle = this.createElement('h4', 'title', 'Humidity');
+        const humidityValueHolder = this.createElement('div', 'valueHolder');
+        const humidityValue = this.createElement('p', 'value', forecast.days[index].humidity);
+        const humidityUnit = this.createElement('p', 'unit', '%');
+        humidityValueHolder.appendChild(humidityValue);
+        humidityValueHolder.appendChild(humidityUnit);
+        humidityCol.appendChild(humidityTitle);
+        humidityCol.appendChild(humidityValueHolder);
+
+        const dewIcon = this.createElement('div', 'dewIcon');
+        const dewCol = this.createElement('div', 'containerCol');
+        const dewTitle = this.createElement('h4', 'title', 'Dew Point');
+        const dewValueHolder = this.createElement('div', 'valueHolder');
+        const dewValue = this.createElement('p', 'value', this.tempToggle === 0 ? forecast.days[index].dew : this.convertC(forecast.days[index].dew));
+        const dewUnit = this.createElement('p', 'unit', this.tempToggle === 0 ? '°F' : '°C');
+        dewValueHolder.appendChild(dewValue);
+        dewValueHolder.appendChild(dewUnit);
+        dewCol.appendChild(dewTitle);
+        dewCol.appendChild(dewValueHolder);
+
+        humidityContainer.appendChild(humidityIcon);
+        humidityContainer.appendChild(humidityCol);
+        humidityContainer.appendChild(dewIcon);
+        humidityContainer.appendChild(dewCol);
+
+        // Wind container
+        const windContainer = this.createElement('div', 'container');
+        const windIcon = this.createElement('div', 'windIcon');
+        const fromCol = this.createElement('div', 'containerCol');
+        const fromTitle = this.createElement('h4', 'title', 'From');
+        const fromValue = this.createElement('p', 'value', Weather.getWindDir(forecast.days[index].winddir));
+        fromCol.appendChild(fromTitle);
+        fromCol.appendChild(fromValue);
+
+        const windDirArrow = this.createElement('div', 'windDirArrow');
+        windDirArrow.classList.add(Weather.getWindDirIcon(Weather.getWindDir(forecast.days[index].winddir)));
+
+        const speedCol = this.createElement('div', 'containerCol');
+        const speedTitle = this.createElement('h4', 'title', 'Speed');
+        const speedValueHolder = this.createElement('div', 'valueHolder');
+        const speedValue = this.createElement('p', 'value', this.speedToggle === 0 ? forecast.days[index].windspeed : this.convertKm(forecast.days[index].windspeed));
+        const speedUnit = this.createElement('p', 'unit', this.speedToggle === 0 ? "mph": "km/h");
+        speedValueHolder.appendChild(speedValue);
+        speedValueHolder.appendChild(speedUnit);
+        speedCol.appendChild(speedTitle);
+        speedCol.appendChild(speedValueHolder);
+
+        const gustCol = this.createElement('div', 'containerCol');
+        const gustTitle = this.createElement('h4', 'title', 'Gust');
+        const gustValueHolder = this.createElement('div', 'valueHolder');
+        const gustValue = this.createElement('p', 'value', this.speedToggle === 0 ? forecast.days[index].windgust : this.convertKm(forecast.days[index].windgust));
+        const gustUnit = this.createElement('p', 'unit', this.speedToggle === 0 ? "mph": "km/h");
+        gustValueHolder.appendChild(gustValue);
+        gustValueHolder.appendChild(gustUnit);
+        gustCol.appendChild(gustTitle);
+        gustCol.appendChild(gustValueHolder);
+
+        windContainer.appendChild(windIcon);
+        windContainer.appendChild(fromCol);
+        windContainer.appendChild(windDirArrow);
+        windContainer.appendChild(speedCol);
+        windContainer.appendChild(gustCol);
+
+        containerWrap3.appendChild(humidityContainer);
+        containerWrap3.appendChild(windContainer);
+
+        if (!forecast.days[index].windgust > 0 || forecast.days[index].windgust < forecast.days[index].wind) {
+            gustCol.style.display = "none";
+        }
+
+        // --- containerWrap: Visibility, UV, Sunrise, Sunset, Moon ---
+        const containerWrap4 = this.createElement('div', 'containerWrap');
+
+        // Visibility & UV
+        const visUVContainer = this.createElement('div', 'container');
+        const visibilityIcon = this.createElement('div', 'visibilityIcon');
+
+        const visibilityCol = this.createElement('div', 'containerCol');
+        const visibilityTitle = this.createElement('h4', 'title', 'Visibility');
+        const visibilityValueHolder = this.createElement('div', 'valueHolder');
+        const visibilityValue = this.createElement('p', 'value', this.speedToggle === 0 ? forecast.days[index].visibility : this.convertKm(forecast.days[index].visibility));
+        const visibilityUnit = this.createElement('p', 'unit', this.speedToggle === 0 ? "mi": "km");
+        visibilityValueHolder.appendChild(visibilityValue);
+        visibilityValueHolder.appendChild(visibilityUnit);
+        visibilityCol.appendChild(visibilityTitle);
+        visibilityCol.appendChild(visibilityValueHolder);
+
+        const uvIcon = this.createElement('div', 'UVindexIcon');
+        const uvCol = this.createElement('div', 'containerCol');
+        const uvTitle = this.createElement('h4', 'title', 'UV Index');
+        const uvValueHolder = this.createElement('div', 'valueHolder');
+        const uvValue = this.createElement('p', 'value', forecast.days[index].uvindex);
+        const uvUnit = this.createElement('p', 'unit', '/10');
+        uvValueHolder.appendChild(uvValue);
+        uvValueHolder.appendChild(uvUnit);
+        uvCol.appendChild(uvTitle);
+        uvCol.appendChild(uvValueHolder);
+
+        visUVContainer.appendChild(visibilityIcon);
+        visUVContainer.appendChild(visibilityCol);
+        visUVContainer.appendChild(uvIcon);
+        visUVContainer.appendChild(uvCol);
+
+        // Sunrise & Sunset
+        const sunContainer = this.createElement('div', 'container');
+        const sunriseIcon = this.createElement('div', 'sunriseIcon');
+        const sunriseCol = this.createElement('div', 'containerCol');
+        const sunriseTitle = this.createElement('h4', 'title', 'Sunrise');
+        const parsedSunrise = parse(forecast.days[index].sunrise, "HH:mm:ss", new Date());
+        const sunriseValue = this.createElement('p', 'value', format(parsedSunrise, "H:mm"));
+        sunriseCol.appendChild(sunriseTitle);
+        sunriseCol.appendChild(sunriseValue);
+
+        const sunsetIcon = this.createElement('div', 'sunsetIcon');
+        const sunsetCol = this.createElement('div', 'containerCol');
+        const sunsetTitle = this.createElement('h4', 'title', 'Sunset');
+        const parsedSunset = parse(forecast.days[index].sunset, "HH:mm:ss", new Date());
+        const sunsetValue = this.createElement('p', 'value', format(parsedSunset, "H:mm"));
+        sunsetCol.appendChild(sunsetTitle);
+        sunsetCol.appendChild(sunsetValue);
+
+        sunContainer.appendChild(sunriseIcon);
+        sunContainer.appendChild(sunriseCol);
+        sunContainer.appendChild(sunsetIcon);
+        sunContainer.appendChild(sunsetCol);
+
+        // Moon
+        const moonContainer = this.createElement('div', 'container');
+        const moonIcon = this.createElement('div', 'currentMoonIcon');
+        moonIcon.classList.add(Weather.getMoonIcon(Weather.getMoonPhase(forecast.days[index].moonphase)));
+        const moonCol = this.createElement('div', 'containerCol');
+        const moonTitle = this.createElement('h4', 'title', 'Moon Phase');
+        const moonValue = this.createElement('p', 'value', Weather.getMoonPhase(forecast.days[index].moonphase));
+        moonCol.appendChild(moonTitle);
+        moonCol.appendChild(moonValue);
+
+        moonContainer.appendChild(moonIcon);
+        moonContainer.appendChild(moonCol);
+
+        // Append all to containerWrap4
+        containerWrap4.appendChild(visUVContainer);
+        containerWrap4.appendChild(sunContainer);
+        containerWrap4.appendChild(moonContainer);
+
+        // --- Append all ---
+        sectionFuture.appendChild(currentHeader);
+        sectionFuture.appendChild(containerWrap1);
+        sectionFuture.appendChild(containerWrap2);
+        sectionFuture.appendChild(containerWrap3);
+        sectionFuture.appendChild(containerWrap4);
+
         content.appendChild(backBtn);
-        content.appendChild(future);
+        content.appendChild(sectionFuture);
         this.redrawHourly();
     }
 }
--- todo.md
+++ todo.md
@@ -1,6 +1,4 @@
-add event handling to click on future dates
-add redrawFuture function
-style Future for mobile
 add light / dark mode styling
 add in any alerts received from API
 refine function to determine weather icon url conditionally
+scroll down = scroll right in containers
